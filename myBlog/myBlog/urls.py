bff59f68571900b603586d97441ea009eef82aa0
Vu Minh Tung (FGA.AIS) <TungVM4@fsoft.com.vn>
2019-09-05 16:22:30 +0700

    abc

--- myBlog/myBlog/urls.py
+++ myBlog/myBlog/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.contrib import myapp
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('hello/', myapp.view.hello,name = 'hello'),
 ]
